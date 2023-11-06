1e2dfe29290252c0fcb2b48ea654c84b27a5679a
Cypher Firelair <gaetan.l@protonmail.com>
2023-11-06 16:50:07 +0100

    Initial commit with call to smwyg-api

---
+++ modules/config.js
@@ -0,0 +1,6 @@
+export const config = {
+    //api_key: '91b96d8d3dc851fb01aa9a36e8c81880',
+    //api_base_url: 'https://api.themoviedb.org/3/',
+    api_base_url: 'http://localhost:8080',
+    image_base_url: 'https://image.tmdb.org/t/p/w1280'
+}
